6d699571a8dcc8079dab918c91d1811d4ea27a0a
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-02-01 13:39:47 -0300

    Payment, one-to-one association

--- src/main/java/com/gianprog/primeirospring/config/TestConfig.java
+++ src/main/java/com/gianprog/primeirospring/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.gianprog.primeirospring.entities.Category;
 import com.gianprog.primeirospring.entities.Order;
 import com.gianprog.primeirospring.entities.OrderItem;
+import com.gianprog.primeirospring.entities.Payment;
 import com.gianprog.primeirospring.entities.Product;
 import com.gianprog.primeirospring.entities.User;
 import com.gianprog.primeirospring.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
