127a510d6856bf0c7c5b2a75b6067b33ccf8a49a
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-02-01 17:45:44 -0300

    User insert

--- src/main/java/com/gianprog/primeirospring/resources/UserResource.java
+++ src/main/java/com/gianprog/primeirospring/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.gianprog.primeirospring.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gianprog.primeirospring.entities.User;
 import com.gianprog.primeirospring.services.UserService;
@@ -15,26 +19,35 @@ import com.gianprog.primeirospring.services.UserService;
 // Camadas de recursos
 // Esta classe vai me disponibilizar um recurso web correspondente à intidede User
 
-
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
+
+	// Serve para retornar um User se chamado pelo método Get
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
+	// Caso eu inclua uma /{id} na pesquisa ele me retorna o User correspondente à
+	// aquele Id
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User obj = service .findById(id);
+		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
-	}
-	
-	
-}
 
+	}
+
+	// Para inserir um novo User, eu devo usar o método Post
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+}
